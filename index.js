1b3427d84131fc6fa4466df46e5dbe2ba920dcfc
Vincent <vincent854321@gmail.com>
2023-10-02 16:51:00 -0500

    script waits for dom to load then scrapes page

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const button = document.getElementById("button");
-button.addEventListener("click", function(){
-    alert("hello was up");
+button.addEventListener("click", async function () {
+    let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+    })
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+            function onDOMReady() {
+                const emailRegEx = /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/g;
+
+                const emails = document.body.innerHTML.match(emailRegEx);
+
+                // Now you can use 'emails' to work with the matched email addresses
+                if (emails) {
+                    alert(emails);
+                } else {
+                    console.log("No email addresses found on this page.");
+                }
+            }
+
+            // Check if the DOM is already ready, and if not, wait for it
+            if (document.readyState === "loading") {
+                document.addEventListener("DOMContentLoaded", onDOMReady);
+            } else {
+                onDOMReady();
+            }
+        },
+    })
 })
+
+
 
 
